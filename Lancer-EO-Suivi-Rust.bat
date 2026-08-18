4f28bd4fef21f6e7e90e9d15f5492d3702fe4570
vx450 <vx4500@gmail.com>
2026-08-18 07:18:14 +0200

    Delete Lancer-EO-Suivi-Rust.bat

--- Lancer-EO-Suivi-Rust.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-setlocal
-cd /d "%~dp0"
-set "ELEVAGE_DATA=%LOCALAPPDATA%\EO-Suivi-Elevage\data"
-if not exist "%ELEVAGE_DATA%" mkdir "%ELEVAGE_DATA%"
-start "EO-Suivi Rust" /min EO-Suivi-Rust.exe
-timeout /t 2 /nobreak >nul
-start "" http://127.0.0.1:8080
